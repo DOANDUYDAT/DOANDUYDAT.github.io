6df084e42fffb501d1ade9ed1cc6b20ac6ce549d
Doan Duy Dat <doanduydat98@gmail.com>
2019-02-19 21:11:03 +0700

    continue

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ const navSlide = () => {
     const navLinks = document.querySelectorAll('.nav-links li');
     burger.addEventListener('click', () => {
         //toggle nav
-        nav.classList.toggle('nav-active');
-
+        nav.classList.toggle('nav-change');
+        // burger animation
+        burger.classList.toggle('toggle');
         //animate links
         navLinks.forEach((link, index) => {
             if (link.style.animation) {
@@ -14,14 +15,11 @@ const navSlide = () => {
                 link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
             }
         });
-
-        // burger animation
-        burger.classList.toggle('toggle');
     });
-
+    
     nav.addEventListener('click', () => {
         //toggle nav
-        nav.classList.toggle('nav-active');
+        nav.classList.toggle('nav-change');
         // burger animation
         burger.classList.toggle('toggle');
         navLinks.forEach((link, index) => {
@@ -37,25 +35,15 @@ const navSlide = () => {
 navSlide();
 
 
-// window.onscroll = function () {
-//     scrollFunction()
-// };
+window.onscroll = function () {
+    scrollFunction()
+};
 
-// function scrollFunction() {
-//     const navA = document.querySelectorAll('.navbar a');
-//     const logo = document.querySelector('.logo');
-//     const navbar = document.querySelector('.navbar');
-//     if (document.documentElement.scrollTop > 100) {
-//         navbar.style.backgroundColor = "#e0ebe8";
-//         logo.style.color = "#0c9b9b";
-//         navA.forEach(a => {
-//             a.style.color = "#0c9b9b";
-//         })
-//     } else {
-//         navbar.style.backgroundColor = "transparent";
-//         logo.style.color = "#e0ebe8";
-//         navA.forEach(a => {
-//             a.style.color = "#e0ebe8";
-//         })
-//     }
-// }
+function scrollFunction() {
+    const navbar = document.querySelector('.navbar');
+    if (document.documentElement.scrollTop > 100) {
+        navbar.style.backgroundColor = "#e0ebe8";
+    } else {
+        navbar.style.backgroundColor = "transparent";
+    }
+}
